331ccdc081cbb615f09dac4bcaf93a55fde0091f
muhengzhang <61950314+muhengzhang@users.noreply.github.com>
2020-10-23 18:30:10 +0800

    Add files via upload
    Add all codes

---
+++ app/src/main/java/com/example/tinnews/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.tinnews;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+import android.os.Bundle;
+
+import com.example.tinnews.network.NewsApi;
+import com.example.tinnews.network.RetrofitClient;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+
+
+public class MainActivity extends AppCompatActivity {
+    private NavController navController;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        navController = navHostFragment.getNavController();
+        NavigationUI.setupWithNavController(navView, navController);
+        NewsApi api = RetrofitClient.newInstance(this).create(NewsApi.class);
+
+
+    }
+}
